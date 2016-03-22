30f14b833b01509ef01b3a8d71407c07d2940ace
Ethan Rubio <ethanrubio@gmail.com>
2016-03-22 15:58:06 -0700

    [feature] adds socket requirements

--- client/typings/browser.d.ts
+++ client/typings/browser.d.ts
@@ -1,2 +1,5 @@
 /// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/ambient/socket.io-client/index.d.ts" />
+/// <reference path="browser/ambient/socket.io/index.d.ts" />
 /// <reference path="browser/ambient/underscore/index.d.ts" />
